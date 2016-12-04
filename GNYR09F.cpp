32749517ec112ad54b8b4378be227664632fd33d
VAIBHAV CHAUHAN <vaibhavchauhan94.vc@gmail.com>
2016-12-05 04:30:09 +0530

    Fixing stash issues

--- GNYR09F.cpp
+++ GNYR09F.cpp
@@ -27,7 +27,9 @@ void init()
                 }
             }
         }
-        //Base Case Initialization
+
+        //SDFDS
+
         arr[2][0][1]=1;
         arr[2][0][0]=2;
         arr[2][1][0]=0;
